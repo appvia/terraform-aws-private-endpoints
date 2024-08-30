f550c74180504b5348e085da7e277f212209994b
Rohith Jayawardene <gambol99@gmail.com>
2024-08-30 19:00:03 +0100

    chore: ensuring we have tags on the security group rules

--- main.tf
+++ main.tf
@@ -64,6 +64,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https_ingress" {
   from_port         = 443
   ip_protocol       = "tcp"
   security_group_id = aws_security_group.this.id
+  tags              = var.tags
   to_port           = 443
 }
 
@@ -74,5 +75,6 @@ resource "aws_vpc_security_group_egress_rule" "allow_https_egress" {
   from_port         = 443
   ip_protocol       = "tcp"
   security_group_id = aws_security_group.this.id
+  tags              = var.tags
   to_port           = 443
 }
